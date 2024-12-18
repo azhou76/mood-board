45dece0836a4ba24cc9e6e0f5b641ebb4b4ef890
Shravya Sompalli <90432005+ssompal1@users.noreply.github.com>
2024-12-17 22:54:22 -0500

    debugging flappy tests

--- main/GameModes.h
+++ main/GameModes.h
@@ -16,10 +16,9 @@ void handleModeNormal(int soundValue);
 void handleModeBright(int soundValue);
 void handleModeParty(int soundValue);
 void handleModeGame();
-
-void mockHandleModeGame(long currentTime, int clapValue,int flapTime,
-                        int lastUpdateTime, int updateInterval, bool isGameOver,
-                        int birdX, int BirdY,int obstacleX, int obstacleGap);
-void mockUpdateGame(int birdX, int obstacleX, int obstacleGap, int score, int gravity, int birdY, int isGameOver);
+void mockUpdateGame(int &birdX, int &obstacleX, int &obstacleGap, &int score, int &gravity, &int birdY, &int isGameOver);
+void mockHandleModeGame(long &currentTime, int &clapValue, int &flapTime,
+                        int &lastUpdateTime, int updateInterval, bool &isGameOver,
+                        int &birdX, int &birdY, int &obstacleX, int &obstacleGap, int &gravity, int &score);
 
 #endif
